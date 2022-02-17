42d1cc44dae181fc7261745fcdb3314425add218
David Li <dvdli2323@gmail.com>
2022-02-17 16:31:11 -0500

    created basic post card, categories, and post widget components

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+
+const post = [
+  { title: 'React Testing', exercpt: 'Learn React Testing'},
+  { title: 'React with Tailwind', exercpt: 'Learn React with Tailwind'}
+]
+
+export default function Home() {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <Head>
+        <title>Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {post.map((post) => <PostCard post={post} key={post.title} />)}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
