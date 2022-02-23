316cb0b6148ffb3e4c7a14187163f67e05cc41db
David Li <dvdli2323@gmail.com>
2022-02-23 16:32:16 -0500

    created dummy categories for header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
+
+const categories = [{name: 'React', slug: 'react'}, {name: 'Web Development', slug: 'web-dev'}]
 
 const Header = () => {
   return (
@@ -13,7 +15,13 @@ const Header = () => {
           </Link>
         </div>
         <div className='hidden md:float-left md:contents'>
-
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                {category.name}
+              </span>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
