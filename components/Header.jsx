d32c5e10fa57d405a6f766e705804bdaca92fa96
David Li <dvdli2323@gmail.com>
2022-02-23 17:08:41 -0500

    finished setting up

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,15 +9,15 @@ const Header = () => {
       <div className='border-b w-full inline-block border-blue-400 py-8'>
         <div className='md:float-left block'>
           <Link href='/'>
-            <span className='cursor-pointer font-bold text-4xl text-white'>
-              Blog
+            <span className='cursor-pointer font-bold text-4xl text-black'>
+              Correo
             </span>
           </Link>
         </div>
         <div className='hidden md:float-left md:contents'>
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+              <span className='md:float-right mt-2 align-middle text-[#505E64] ml-4 font-semibold cursor-pointer'>
                 {category.name}
               </span>
             </Link>
