954165df6b6759289dd57ab035b5f5018841bbe7
David Li <dvdli2323@gmail.com>
2022-02-23 16:44:02 -0500

    added header as top component for layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
 // import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
