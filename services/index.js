df14c665be1e0b7dcf3addd57587fd7065debe93
David Li <dvdli2323@gmail.com>
2022-02-24 12:32:09 -0500

    fetched first query from graphcms

---
+++ services/index.js
@@ -0,0 +1,35 @@
+import { request, gql } from 'graphql-request'; // make graph queries
+
+export const getPost = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `
+}
+
+
