864b8aaab4a3f180262919b45815b7b8ac88f8c8
David Li <dvdli2323@gmail.com>
2022-03-03 12:15:21 -0500

    added post excerpt and tailwind css button

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -29,6 +29,14 @@ const PostCard = ({ post }) => {
           </span>
         </div>
       </div>
+      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
+      <div className='text-center'>
+        <Link href={`/post/${post.slug}`}>
+          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+            Continue Reading
+          </span>
+        </Link>
+      </div>
     </div>
   )
 }
