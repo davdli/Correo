448e2bf7c42f565ed4c70ec92ce96b3bd15a1131
David Li <dvdli2323@gmail.com>
2022-03-09 12:17:44 -0500

    fetching data from graphcms from graphql query

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getCategories } from '../services';
 
 const Categories = () => {
+  const [categories, setCategories] = useState([])
+
+  // fetch data from graphcms from graphql query
+  useEffect(() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, [])
+
   return (
     <div>
       Categories
