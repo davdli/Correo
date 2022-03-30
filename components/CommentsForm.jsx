1d470e824994b4b51440a30225e80b356fec5539
David Li <dvdli2323@gmail.com>
2022-03-30 12:04:55 -0400

    created text area in comments section

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setlocalStorage] = useState(null);
+  const [showSuccessMessage, setshowSuccessMessage] = useState(false);
+
+  // read value from input field and send to graphcms
+  // refs more efficient than tracking of entire input state
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
   return (
-    <div>
-      <h1>CommentsForm</h1>
+    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>CommentsForm</h3>
+      <div className='grid grid-cols-1 gap-4 mb-4'>
+        <textarea ref={commentEl} className='p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100'>
+
+        </textarea>
+      </div>
     </div>
   )
 }
