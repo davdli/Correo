430b07b9b9bb2d7a450f9acf15429e17c5c9e690
David Li <dvdli2323@gmail.com>
2022-03-31 09:17:54 -0400

    displays inputs for name and email in comments form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -16,9 +16,28 @@ const CommentsForm = ({ slug }) => {
     <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>CommentsForm</h3>
       <div className='grid grid-cols-1 gap-4 mb-4'>
-        <textarea ref={commentEl} className='p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100'>
-
-        </textarea>
+        <textarea
+          ref={commentEl}
+          className='p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100'
+          placeholder='Commment'
+          name='comment'
+        />
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
+        <input
+          type='text'
+          ref={nameEl}
+          className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100'
+          placeholder='Name'
+          name='name'
+        />
+         <input
+          type='text'
+          ref={emailEl}
+          className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100'
+          placeholder='Email'
+          name='email'
+        />
       </div>
     </div>
   )
