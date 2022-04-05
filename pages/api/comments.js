573ede303c4f470e645eb8a1775c12c45d3331f0
David Li <dvdli2323@gmail.com>
2022-04-05 09:12:10 -0400

    created query to connect name, email to post that user comments on

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -6,12 +6,22 @@ import { GraphQLClient, gql } from 'graphql';
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 // how we authenticate our graphql client from creating auth token from graphcms
-export default function comments(req, res) {
+export default async function comments(req, res) {
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
       authorization: 'Bearer ${process.env.GRAPHCMS_TOKEN}'
     }
   })
-  res.status(200).json({ name: 'John Doe'})
+  // connecting comment, name, email to specific post that the user commented on
+  // once we run query, it will create comment in graph cms dashboard
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slugL String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug }}}) {id}
+    }
+  `
+
+  const result = await graphQLClient.request(query, req.body)
+
+  return res.status(200).send(result);
 }
 
