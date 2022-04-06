1020c787ad04f1fb8871a48a98e110de75bc5b66
David Li <dvdli2323@gmail.com>
2022-04-06 09:19:50 -0400

    error when posting comment, fixing api endpoint

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { submitComment } from '../services'
 
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
@@ -11,6 +12,11 @@ const CommentsForm = ({ slug }) => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name');
+    emailEl.current.value = window.localStorage.getItem('email');
+  }, [])
 
   const handleCommentSubmission = () => {
     setError(false);
@@ -28,12 +34,21 @@ const CommentsForm = ({ slug }) => {
     const commentObj = { name, email, comment, slug }
 
     if (storeData) {
-      localStorage.setItem('name', name);
-      localStorage.setItem('email', email);
+      window.localStorage.setItem('name', name);
+      window.localStorage.setItem('email', email);
     } else {
-      localStorage.removeItem('name', name);
-      localStorage.removeItem('email', email)
+      window.localStorage.removeItem('name', name);
+      window.localStorage.removeItem('email', email)
     }
+
+    submitComment(commentObj)
+      .then((res) => {
+        setshowSuccessMessage(true);
+
+        setTimeout(() => {
+          setshowSuccessMessage(false);
+        }, 3000);
+      })
   }
 
   return (
